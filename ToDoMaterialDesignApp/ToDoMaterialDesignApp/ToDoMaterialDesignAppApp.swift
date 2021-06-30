fa4ccb4fcb7620289b8a4b8b06041a2f3dd65a87
Matthias Zarzecki <matthias.zarzecki@gmail.com>
2021-06-30 08:13:22 +0200

    Add project

---
+++ ToDoMaterialDesignApp/ToDoMaterialDesignApp/ToDoMaterialDesignAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoMaterialDesignAppApp.swift
+//  ToDoMaterialDesignApp
+//
+//  Created by Matthias Zarzecki on 30.06.21.
+//
+
+import SwiftUI
+
+@main
+struct ToDoMaterialDesignAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
